9034d21f8ab1681b2cc50ebd9de63f7c3da5f3ed
Casey K <caseykelleher@icloud.com>
2020-01-15 16:19:43 -0500

    added files

---
+++ bamazonCustomer.js
@@ -0,0 +1,113 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "Austin316!",
+    database: "bAmazon_DB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    displayProducts()
+});
+
+// Display all the items available for sale
+function displayProducts() {
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        // Log all results 
+        for (var i = 0; i < res.length; i++) {
+            console.log("ID: " + res[i].id + " | " + "Product: " + res[i].product_name + " | " + "Department: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "Quantity: " + res[i].stock_quantity);
+            console.log('------------------------------------------------------------------------------------------------------------')
+        };
+        selectProducts(res)
+    });
+};
+
+// Two user prompts: 1) ask ID of product they want to buy, 2) ask how many units they want to buy
+function selectProducts(inventory) {
+    inquirer.prompt([{
+            name: "id",
+            type: "input",
+            message: "What is the ID of the product you would like to purchase?\n",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            },
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many would you like to purchase?\n",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            },
+        },
+    ]).then(function (answer) {
+        console.log("Updating product stock...\n");
+        var chosenProduct;
+        for (var i = 0; i < inventory.length; i++) {
+            if (inventory[i].id === parseInt(answer.id)) {
+                chosenProduct = inventory[i];
+            };
+        };
+        if (!chosenProduct) {
+            console.log("Sorry, we do not carry that item.");
+            startOver()
+        };
+        // After order is placed, check to see if enough inventory. 
+        // If so, fulfill the order: 1) update database with new quantity, 2) display total cost of purchase
+        if (chosenProduct) {
+            if (chosenProduct.stock_quantity >= parseInt(answer.quantity)) {
+                connection.query(
+                    "UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ?",
+                    [
+                        answer.quantity, chosenProduct.id
+                    ],
+                    function (err) {
+                        if (err) throw err;
+                        console.log("Thank you for purchasing" + chosenProduct.product_name + ". You have purchased " + answer.quantity + ".");
+                        startOver()
+                    }
+                );
+                // If not, notify with alert.
+            } else {
+                console.log("We do not have that many.");
+                startOver()
+            };
+        };
+    });
+};
+
+function startOver() {
+    inquirer.prompt([{
+        name: "confirm",
+        type: "confirm",
+        message: "Would you like to purchase another item?"
+    }]).then(function (answer) {
+        if (answer.confirm) {
+            displayProducts()
+        } else {
+            console.log("Thank you for shopping at Bamazon. Have a great day.");
+            process.exit()
+        };
+    });
+}
